2a795ecd9593915bc08684b4c4231fa64a4d44bc
Vicente D. Boyer <50031566+vicentedboyer@users.noreply.github.com>
2024-07-23 12:30:33 -0400

    Update SalaryCalculator.java

--- src/Chapter3/SalaryCalculator.java
+++ src/Chapter3/SalaryCalculator.java
@@ -6,23 +6,19 @@ public class SalaryCalculator {
 
     public static void main(String args[]){
 
-        //Initialize known values
         int salary = 1000;
         int bonus = 250;
         int quota = 10;
 
-        //Get values for the unknown
         System.out.println("Cuántas ventas realizó el empleado esta semana?");
         Scanner scanner = new Scanner(System.in);
         int sales = scanner.nextInt();
         scanner.close();
 
-        //Quick detour for the bonus earners
         if(sales > quota){
             salary = salary + bonus;
         }
 
-        //Output
         System.out.println("El salario del empleado es $" + salary);
 
     }
