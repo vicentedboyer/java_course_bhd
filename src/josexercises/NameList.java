cf474ae68b90a58aa09f6f1c9c05c282430bc74b
vicentedboyer <vd.boyer1415@gmail.com>
2024-07-25 09:08:14 -0400

    V6

---
+++ src/josexercises/NameList.java
@@ -0,0 +1,21 @@
+package josexercises;
+
+import java.util.ArrayList;
+
+public class NameList {
+    public static void main(String args[]){
+        //Creo el array para almacenar los nombres.
+        ArrayList<String> names = new ArrayList<>();
+
+        //Agrego los nombres.
+        names.add("Aleanny");
+        names.add("Vicente");
+        names.add("Juan");
+        names.add("Carlos");
+
+        //Recorrer los nombres del Array y imprimirlos.
+        for (String name : names){
+            System.out.println(name);
+        }
+    }
+}
