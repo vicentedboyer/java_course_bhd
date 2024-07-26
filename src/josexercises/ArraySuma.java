cc53f28f1056fe8358f2604cf4f6e3b3a317c94f
vicentedboyer <vd.boyer1415@gmail.com>
2024-07-26 08:02:41 -0400

    V7 Jose Exercices.

---
+++ src/josexercises/ArraySuma.java
@@ -0,0 +1,27 @@
+package josexercises;
+
+public class ArraySuma {
+    public static void main(String[] args) {
+        // Definir un array de enteros
+        int[] array = {1, 2, 3, 4, 5};
+
+        // Llamar a la función para sumar los elementos del array
+        int sum = sumArray(array);
+
+        // Imprimir la suma de los elementos del array
+        System.out.println("La suma del array es: " + sum);
+    }
+
+    // Función para sumar los elementos de un array
+    public static int sumArray(int[] array) {
+        int sum = 0;
+
+        // Iterar a través del array y sumar cada elemento
+        for (int num : array) {
+            sum += num;
+        }
+
+        return sum;
+    }
+}
+
